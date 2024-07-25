05ab6da841284ba93e14d4571f6fcd77d7c8bf60
Cleyton Muto <cleytonmuto@gmail.com>
2024-07-25 12:15:51 -0300

    refactor: app.tsx e main.tsx refatorados

--- src/app.tsx
+++ src/app.tsx
@@ -1,29 +1,22 @@
-import './global.css'
+import './global.css';
 
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes'
-import { Helmet, HelmetProvider } from 'react-helmet-async'
-import { Toaster } from 'sonner'
-import { ThemeProvider } from './components/theme/theme-provider'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from './lib/react-query'
+import { QueryClientProvider } from '@tanstack/react-query';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { RouterProvider } from 'react-router-dom';
+import { Toaster } from 'sonner';
 
-export function App() {
-    
-  return (
+import { ThemeProvider } from './components/theme/theme-provider';
+import { queryClient } from './lib/react-query';
+import { router } from './routes';
+
+export const App = () => (
    <HelmetProvider> 
     <ThemeProvider storageKey="lexpge-theme" defaultTheme="dark">
       <Helmet titleTemplate="LEXPGE | %s" />
       <Toaster richColors />
-
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
-      
     </ThemeProvider>
    </HelmetProvider>
-    
-  )
-}
-
-
+);
