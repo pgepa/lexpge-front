19ece8293906bf939603889fbef6482fd6e47970
Cleyton Muto <cleytonmuto@gmail.com>
2024-07-25 12:20:11 -0300

    refactor: sobre.tsx e 404.tsx refatorados

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-export function NotFound() {
-  return (
-    <div className="flex h-screen flex-col items-center justify-center gap-2">
-      <h1 className="text-4xl font-bold">Página não encontrada</h1>
-      <p className="text-accent-foreground">
-        Voltar para a página <Link to="/" className="text-violet-600 dark:text-violet-400">Atos Normativos</Link>
-      </p>
-    </div>
-  )
-}
+export const NotFound = () => (
+  <div className="flex h-screen flex-col items-center justify-center gap-2">
+    <h1 className="text-4xl font-bold">Página não encontrada</h1>
+    <p className="text-accent-foreground">
+      Voltar para a página <Link to="/" className="text-violet-600 dark:text-violet-400">Atos Normativos</Link>
+    </p>
+  </div>
+);
--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -1,21 +1,18 @@
-import { Helmet } from "react-helmet-async";
-import logo from '@/assets/logo.svg'
+import { Helmet } from 'react-helmet-async';
 
-export function Sobre() {
-  return (
-    <>
-      <Helmet title="Sobre"/>
-      <div className="flex flex-col gap-4 items-center">
-        <img className="w-24 h-24" src={logo} alt="Logo" />
-        <h1 className="text-3xl font-bold tracking-tight text-justify" >Base de Atos Normativos - LEXPGE</h1>
-        <p className="text-muted-foreground">
+import logo from '@/assets/logo.svg';
+
+export const Sobre = () => (
+  <>
+    <Helmet title="Sobre"/>
+    <div className="flex flex-col gap-4 items-center">
+      <img className="w-24 h-24" src={logo} alt="Logo" />
+      <h1 className="text-3xl font-bold tracking-tight text-justify" >Base de Atos Normativos - LEXPGE</h1>
+      <p className="text-muted-foreground">
           O LexPGE iniciou os trabalhos em novembro de 2020, com a inserção de atos normativos publicados diariamente no Diário Oficial do Estado (DOE). 
           Os atos normativos de anos anteriores estão sendo tratados e inseridos retrospectivamente. Atualmente o sistema disponibiliza os atos normativos de 2004 a 2024. 
           Alguns atos normativos de anos anteriores a 2004 podem ser encontrados no LexPGE devido a correlação com atos já inseridos no sistema.
-        </p>
-        
-      </div>
-
-    </>
-  )
-}
+      </p>
+    </div>
+  </>
+);
