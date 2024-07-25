af0c6953536bc201ce18a906d5f029923d4124b2
Cleyton Muto <cleytonmuto@gmail.com>
2024-07-25 15:19:06 -0300

    fix: alterado interface para type alias em atos-card.tsx

--- src/pages/app/atos/atos-card-teste.tsx
+++ src/pages/app/atos/atos-card-teste.tsx
@@ -1,46 +1,27 @@
-import { Helmet } from 'react-helmet-async'
-import { AtosCard } from './atos-card'
-import { AtosTableFilters } from './atos-table-filters'
-import { Pagination } from '@/components/pagination'
-import { NavLink } from 'react-router-dom'
-import { Button } from '@/components/ui/button'
-import { CirclePlus } from 'lucide-react'
+import { Helmet } from 'react-helmet-async';
+import { AtosCard } from './atos-card';
+import { AtosTableFilters } from './atos-table-filters';
+import { Pagination } from '@/components/pagination';
+import { NavLink } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+import { CirclePlus } from 'lucide-react';
 
-
-
-
-export function Atos() {
-
- 
-  return (
-    <>
-      <Helmet title="Atos Normativos" />
-
-      
-
-      <div className="flex flex-col gap-4">
-        <div className="flex justify-between">
-          <h1 className="text-3xl font-bold tracking-tight">Atos Normativos</h1>
-          <NavLink to="/registro">
-              <Button type="submit" variant="default" size="sm">
-                <CirclePlus className="h-4 w-4 mr-2" />
-                Novo Registro
-              </Button>
-            </NavLink>
-
-        </div>
-
-        <div>
-        </div>
-
-        <AtosTableFilters />
-
-        <AtosCard />
-                       
-
-        <Pagination pageIndex={0} totalCount={18750} perPage={10}/>
-         
+export const Atos = () => (
+  <>
+    <Helmet title="Atos Normativos" />
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold tracking-tight">Atos Normativos</h1>
+        <NavLink to="/registro">
+          <Button type="submit" variant="default" size="sm">
+            <CirclePlus className="mr-2 h-4 w-4" />
+            Novo Registro
+          </Button>
+        </NavLink>
       </div>
-    </>
-  )
-}
+      <AtosTableFilters />
+      <AtosCard />
+      <Pagination pageIndex={0} totalCount={18750} perPage={10} />
+    </div>
+  </>
+);
--- src/pages/app/atos/atos-card.tsx
+++ src/pages/app/atos/atos-card.tsx
@@ -1,103 +1,129 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Eye, PencilLine, SquareArrowOutUpRight, Trash2 } from "lucide-react";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Eye, PencilLine, SquareArrowOutUpRight, Trash2 } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-interface AtoCard {
-    id: number;
-    titulo: string;
-    situacao: string;
-    ementa: string;
+export type AtoCard = {
+  id: number;
+  titulo: string;
+  situacao: string;
+  ementa: string;
 }
 
-export function AtosCard() {
-    const [atos, setAtos] = useState<AtoCard[]>([]);
-    const navigate = useNavigate();
+export const AtosCard = () => {
+  const [atos, setAtos] = useState<AtoCard[]>([]);
+  const navigate = useNavigate();
 
-    async function loadAtosCard() {
-        const response = await fetch('http://10.96.20.14:4000/atos');
-        const data = await response.json();
-        setAtos(data);
-    }
+  async function loadAtosCard() {
+    const response = await fetch("http://10.96.20.14:4000/atos");
+    const data = await response.json();
+    setAtos(data);
+  }
 
-    useEffect(() => {
-        loadAtosCard();
-    }, []);
+  useEffect(() => {
+    loadAtosCard();
+  }, []);
 
-    const handleFichaClick = (ato: AtoCard) => {
-        navigate(`/ficha/${ato.id}`, { state: { ato } });
-    };
+  const handleFichaClick = (ato: AtoCard) => {
+    navigate(`/ficha/${ato.id}`, { state: { ato } });
+  };
 
-    const handleTextoIntegralClick = (ato: AtoCard) => {
-        navigate(`/texto-integral/${ato.id}`, { state: { ato } });
-    };
+  const handleTextoIntegralClick = (ato: AtoCard) => {
+    navigate(`/texto-integral/${ato.id}`, { state: { ato } });
+  };
 
-    const handleEditClick = (ato: AtoCard) => {
-        navigate(`/editar/${ato.id}`, { state: { ato } });
-    };
+  const handleEditClick = (ato: AtoCard) => {
+    navigate(`/editar/${ato.id}`, { state: { ato } });
+  };
 
-    const handleDeleteClick = async (id: number) => {
-        if (window.confirm('Tem certeza de que deseja excluir este registro?')) {
-            try {
-                const response = await fetch(`http://10.96.20.14:4000/atos/${id}`, {
-                    method: 'DELETE',
-                });
+  const handleDeleteClick = async (id: number) => {
+    if (window.confirm("Tem certeza de que deseja excluir este registro?")) {
+      try {
+        const response = await fetch(`http://10.96.20.14:4000/atos/${id}`, {
+          method: "DELETE",
+        });
 
-                if (response.ok) {
-                    setAtos(atos.filter(ato => ato.id !== id));
-                    alert('Registro excluído com sucesso.');
-                } else {
-                    alert('Erro ao excluir o registro.');
-                }
-            } catch (error) {
-                console.error('Erro ao excluir o registro:', error);
-                alert('Erro ao excluir o registro.');
-            }
+        if (response.ok) {
+          setAtos(atos.filter((ato) => ato.id !== id));
+          alert("Registro excluído com sucesso.");
+        } else {
+          alert("Erro ao excluir o registro.");
         }
-    };
+      } catch (error) {
+        console.error("Erro ao excluir o registro:", error);
+        alert("Erro ao excluir o registro.");
+      }
+    }
+  };
 
-    return (
-        <>
-            {atos.map((ato) => (
-                <Card key={ato.id}>
-                    <CardHeader className="flex-row space-y-0 flex-items-center justify-between pb-4">
-                        <div className="space-y-1">
-                            <CardTitle className="text-base -tracking-tight font-medium text-blue-700 dark:text-blue-300">{ato.titulo}</CardTitle>
-                            <CardDescription>{ato.situacao}</CardDescription>
-                        </div>
-                    </CardHeader>
-                    <CardContent className="space-y-1">
-                        <span>
-                            <p className="leading-7 [&:not(:first-child)]:mt-6">{ato.ementa}</p>
-                        </span>
-                    </CardContent>
-                    <CardFooter className="flex justify-start gap-2">
-                        <div className="flex flex-row items-center justify-center relative gap-2">
-                            <Button variant="outline" size="xs" className="gap-2 text-amber-500 font-normal border-amber-500 hover:text-amber-600 dark:border-amber-300 dark:text-amber-300" onClick={() => handleFichaClick(ato)}>
-                                <Eye className="h-3 w-3" />
-                                Ficha
-                                <span className="sr-only">Ficha do ato normativo</span>
-                            </Button>
-                            <Button variant="outline" size="xs" className="gap-2 text-amber-500 font-normal border-amber-500 hover:text-amber-600 dark:border-amber-300 dark:text-amber-300" onClick={() => handleTextoIntegralClick(ato)}>
-                                <SquareArrowOutUpRight className="h-3 w-3" />
-                                Texto Integral
-                                <span className="sr-only">Visualizar texto integral do ato normativo</span>
-                            </Button>
-                            <Button variant="default" size="xs" className="gap-2" onClick={() => handleEditClick(ato)}>
-                                <PencilLine className="h-3 w-3" />
-                                Editar
-                                <span className="sr-only">Editar ato normativo</span>
-                            </Button>
-                            <Button variant="destructive" size="xs" className="gap-2" onClick={() => handleDeleteClick(ato.id)}>
-                                <Trash2 className="h-3 w-3" />
-                                Excluir
-                                <span className="sr-only">Excluir ato normativo</span>
-                            </Button>
-                        </div>
-                    </CardFooter>
-                </Card>
-            ))}
-        </>
-    );
+  return (
+    <>
+      {atos.map((ato) => (
+        <Card key={ato.id}>
+          <CardHeader className="flex-items-center flex-row justify-between space-y-0 pb-4">
+            <div className="space-y-1">
+              <CardTitle className="text-base font-medium -tracking-tight text-blue-700 dark:text-blue-300">
+                {ato.titulo}
+              </CardTitle>
+              <CardDescription>{ato.situacao}</CardDescription>
+            </div>
+          </CardHeader>
+          <CardContent className="space-y-1">
+            <span>
+              <p className="leading-7 [&:not(:first-child)]:mt-6">
+                {ato.ementa}
+              </p>
+            </span>
+          </CardContent>
+          <CardFooter className="flex justify-start gap-2">
+            <div className="relative flex flex-row items-center justify-center gap-2">
+              <Button
+                variant="outline"
+                size="xs"
+                className="gap-2 border-amber-500 font-normal text-amber-500 hover:text-amber-600 dark:border-amber-300 dark:text-amber-300"
+                onClick={() => handleFichaClick(ato)}
+              >
+                <Eye className="h-3 w-3" />
+                Ficha
+                <span className="sr-only">Ficha do ato normativo</span>
+              </Button>
+              <Button
+                variant="outline"
+                size="xs"
+                className="gap-2 border-amber-500 font-normal text-amber-500 hover:text-amber-600 dark:border-amber-300 dark:text-amber-300"
+                onClick={() => handleTextoIntegralClick(ato)}
+              >
+                <SquareArrowOutUpRight className="h-3 w-3" />
+                Texto Integral
+                <span className="sr-only">
+                  Visualizar texto integral do ato normativo
+                </span>
+              </Button>
+              <Button
+                variant="default"
+                size="xs"
+                className="gap-2"
+                onClick={() => handleEditClick(ato)}
+              >
+                <PencilLine className="h-3 w-3" />
+                Editar
+                <span className="sr-only">Editar ato normativo</span>
+              </Button>
+              <Button
+                variant="destructive"
+                size="xs"
+                className="gap-2"
+                onClick={() => handleDeleteClick(ato.id)}
+              >
+                <Trash2 className="h-3 w-3" />
+                Excluir
+                <span className="sr-only">Excluir ato normativo</span>
+              </Button>
+            </div>
+          </CardFooter>
+        </Card>
+      ))}
+    </>
+  );
 }
