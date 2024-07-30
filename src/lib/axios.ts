8d59ae83dd429863cf2eb66a19413ddf89cb8eb0
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2024-07-30 14:06:10 -0300

    Alteração do arquivo axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,28 @@
-import { env } from "@/env"
-import axios from "axios"
+import axios from 'axios';
+import { env } from "@/env";  // Certifique-se de que o caminho está correto
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
 });
+
+
+api.interceptors.request.use(
+  config => {
+    
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  response => {
+    
+    return response;
+  },
+  error => {
+    
+    return Promise.reject(error);
+  }
+);
