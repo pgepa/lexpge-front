30a5402efbb1a723ba9935f4e26434442e97042e
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2024-08-16 00:51:50 -0300

    alteração na exibição do usuário

--- src/pages/app/gestao/user-exibicao.tsx
+++ src/pages/app/gestao/user-exibicao.tsx
@@ -4,14 +4,6 @@ import { api } from '@/lib/axios';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { PencilLine, Trash2 } from 'lucide-react';
-import {
-  Pagination,
-  PaginationContent,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
 
 export type UserCard = {
   id: number;
@@ -22,8 +14,6 @@ export type UserCard = {
 
 export const UserCard = () => {
   const [users, setUsers] = useState<UserCard[]>([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
   const [loading, setLoading] = useState(false);
   const [filters] = useState({
     nome: "",
@@ -31,19 +21,16 @@ export const UserCard = () => {
     id_perfil: "",
   });
   const [isFiltering] = useState(false);
-  const limit = 10;
   const navigate = useNavigate();
-  const paginationRange = 5;
 
-  async function loadUserCard(pagina = 1, filters = {}) {
+  async function loadUserCard() {
     setLoading(true);
     try {
       const token = localStorage.getItem('token');
       const route = isFiltering ? '/admin/usuario' : '/auth/users';
       const response = await api.get(route, {
         params: {
-          pagina,
-          limite: limit,
+          limite: 1000, // Obtém um número alto para garantir que todos os dados sejam carregados de uma vez
           ...filters,
         },
         headers: {
@@ -51,18 +38,7 @@ export const UserCard = () => {
         },
       });
 
-      const fetchedUsers = response.data;
-      setUsers(fetchedUsers);
-
-      if (fetchedUsers.length < limit && pagina === 1) {
-        setTotalPages(1); // Apenas uma página se não houver mais dados
-      } else if (fetchedUsers.length < limit) {
-        setTotalPages(pagina); // Última página com menos usuários que o limite
-      } else {
-        setTotalPages(pagina + 1); // Pode haver mais páginas
-      }
-      
-      setCurrentPage(pagina);
+      setUsers(response.data);
     } catch (error) {
       console.error('Erro ao carregar usuários:', error);
     } finally {
@@ -71,8 +47,8 @@ export const UserCard = () => {
   }
 
   useEffect(() => {
-    loadUserCard(currentPage, filters);
-  }, [currentPage, filters]);
+    loadUserCard();
+  }, [filters]);
 
   const handleEditClick = (user: UserCard) => {
     navigate(`/admin/editar/${user.id}`, { state: { user } });
@@ -102,34 +78,6 @@ export const UserCard = () => {
     }
   };
 
-  const handlePageChange = (pagina: number) => {
-    if (pagina > 0 && pagina <= totalPages) {
-      setCurrentPage(pagina);
-    }
-  };
-
-  const renderPaginationItems = () => {
-    let startPage = Math.max(currentPage - Math.floor(paginationRange / 2), 1);
-    const endPage = Math.min(startPage + paginationRange - 1, totalPages);
-    
-    if (endPage - startPage + 1 < paginationRange) {
-      startPage = Math.max(endPage - paginationRange + 1, 1);
-    }
-
-    const pages = [];
-    for (let i = startPage; i <= endPage; i++) {
-      pages.push(
-        <PaginationItem key={i}>
-          <PaginationLink onClick={() => handlePageChange(i)} isActive={currentPage === i}>
-            {i}
-          </PaginationLink>
-        </PaginationItem>
-      );
-    }
-
-    return pages;
-  };
-
   const getPerfilDescription = (id_perfil: number) => {
     switch (id_perfil) {
       case 1:
@@ -146,30 +94,24 @@ export const UserCard = () => {
   return (
     <>
       {loading && <p>Carregando...</p>}
-      {users.map((user) => (
-        <Card key={user.id}>
-          <CardHeader className="flex-items-center flex-row justify-between space-y-0 pb-4">
-            <div className="space-y-1">
-              <CardTitle className="text-base font-medium -tracking-tight text-blue-700 dark:text-blue-300">
-                {user.nome}
-              </CardTitle>
-              <CardDescription>{getPerfilDescription(user.id_perfil)}</CardDescription>
-            </div>
-          </CardHeader>
-          <CardContent className="space-y-1">
-            <p className="leading-7 [&:not(:first-child)]:mt-6">
-              {user.email}
-            </p>
-          </CardContent>
-          <CardFooter className="flex justify-start gap-2">
-            <div className="relative flex flex-row items-center justify-center gap-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+        {users.map((user) => (
+          <Card key={user.id} className="flex flex-col p-4 border border-gray-200 rounded-lg shadow-md">
+            <CardHeader className="flex flex-col space-y-2 pb-4">
+              <CardTitle className="text-lg font-semibold text-blue-700 dark:text-blue-300 truncate">{user.nome}</CardTitle>
+              <CardDescription className="text-sm text-gray-500">{getPerfilDescription(user.id_perfil)}</CardDescription>
+            </CardHeader>
+            <CardContent className="flex-1">
+              <p className="text-sm text-gray-600">{user.email}</p>
+            </CardContent>
+            <CardFooter className="flex gap-2 mt-4">
               <Button
                 variant="default"
                 size="xs"
                 className="gap-2"
                 onClick={() => handleEditClick(user)}
               >
-                <PencilLine className="h-3 w-3" />
+                <PencilLine className="h-4 w-4" />
                 Editar
                 <span className="sr-only">Editar usuário</span>
               </Button>
@@ -179,29 +121,14 @@ export const UserCard = () => {
                 className="gap-2"
                 onClick={() => handleDeleteClick(user.id)}
               >
-                <Trash2 className="h-3 w-3" />
+                <Trash2 className="h-4 w-4" />
                 Desabilitar
                 <span className="sr-only">Desabilitar usuário</span>
               </Button>
-            </div>
-          </CardFooter>
-        </Card>
-      ))}
-      <Pagination className="sticky bottom-0 bg-white py-2">
-        <PaginationContent>
-          {currentPage > 1 && (
-            <PaginationPrevious
-              onClick={() => handlePageChange(1)}
-            >
-              {currentPage === 2 ? 'Primeira Página' : 'Anterior'}
-            </PaginationPrevious>
-          )}
-          {renderPaginationItems()}
-          {currentPage < totalPages && (
-            <PaginationNext onClick={() => handlePageChange(currentPage + 1)} />
-          )}
-        </PaginationContent>
-      </Pagination>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </>
   );
 };
