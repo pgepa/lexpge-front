c6138239e7fc9ac629efe4af3b21a1b29822bd59
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2024-08-22 13:47:32 -0300

    ajuste na responsividade da página incial pública

--- src/pages/home-public.tsx
+++ src/pages/home-public.tsx
@@ -3,24 +3,21 @@ import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
 import { Search, X } from "lucide-react";
-import logo from '@/assets/logo.svg';// Importando o AtosCard
+import logo from '@/assets/logo.svg';
 import { useState } from 'react';
-import { AtosCardPublic } from './app/atosPublic/atos-card-model'
+import { AtosCardPublic } from './app/atosPublic/atos-card-model';
 
 export function HomePublic() {
-  // Estados para armazenar os filtros
   const [numero, setNumero] = useState("");
   const [ano, setAno] = useState("");
   const [tipo, setTipo] = useState("todos");
   const [termosBusca, setTermosBusca] = useState("");
   const [isSearchPerformed, setIsSearchPerformed] = useState(false);
 
-  // Função que realiza a busca e define os filtros
   const handleSearch = () => {
     setIsSearchPerformed(true);
   };
 
-  // Função que limpa os filtros
   const handleClearFilters = () => {
     setNumero("");
     setAno("");
@@ -31,82 +28,84 @@ export function HomePublic() {
 
   return (
     <>
-      <Helmet title="Início"/>
+      <Helmet title="Início" />
 
       {!isSearchPerformed ? (
-        <div className="flex flex-col gap-4 items-center"> 
+        <div className="flex flex-col gap-4 items-center p-4 sm:p-6 md:p-8">
           <img className="w-24 h-24" src={logo} alt="Logo" />
-          <h1 className="text-3xl font-bold tracking-tight col-span-2">Procuradoria-Geral do Estado do Pará</h1>
-          <h2 className="text-xl tracking-tight text-muted-foreground">Base de Atos Normativos - LEXPGE</h2>
-          
-          <div className="grid grid-cols-4 gap-4">
-            <Input 
-              placeholder="Número" 
-              value={numero} 
-              onChange={(e) => setNumero(e.target.value)} 
+          <h1 className="text-2xl sm:text-3xl font-bold text-center mb-2">Procuradoria-Geral do Estado do Pará</h1>
+          <h2 className="text-lg sm:text-xl tracking-tight text-muted-foreground">Base de Atos Normativos - LEXPGE</h2>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 w-full max-w-4xl">
+            <Input
+              placeholder="Número"
+              value={numero}
+              onChange={(e) => setNumero(e.target.value)}
+              className="w-full"
             />
-            <Input 
-              placeholder="Ano" 
-              value={ano} 
-              onChange={(e) => setAno(e.target.value)} 
+            <Input
+              placeholder="Ano"
+              value={ano}
+              onChange={(e) => setAno(e.target.value)}
+              className="w-full"
             />
-            <div className="col-span-2">
-              <Select 
-                value={tipo} 
-                onValueChange={(value) => setTipo(value)}
-              >
-                <SelectTrigger>
-                  <SelectValue />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="todos">Todos os tipos</SelectItem>
-                  <SelectItem value="constituicaoEstadual">Constituição Estadual</SelectItem>
-                  <SelectItem value="decretoLegislativo">Decreto Legislativo</SelectItem>
-                  <SelectItem value="decretoLei">Decreto Lei</SelectItem>
-                  <SelectItem value="decretoNumerado">Decreto Numerado</SelectItem>
-                  <SelectItem value="decretoNaoNumerado">Decreto Não Numerado</SelectItem>
-                  <SelectItem value="emendaConsitucional">Emenda Constitucional</SelectItem>
-                  <SelectItem value="instrucaoNormativa">Instrução Normativa</SelectItem>
-                  <SelectItem value="leiComplementar">Lei Complementar</SelectItem>
-                  <SelectItem value="leiOrdinaria">Lei Ordinária</SelectItem>
-                  <SelectItem value="mensagemDoGovernador">Mensagem do Governador</SelectItem>
-                  <SelectItem value="portaria">Portaria</SelectItem>
-                  <SelectItem value="portariaConjunta">Portaria Conjunta</SelectItem>
-                  <SelectItem value="resolucao">Resolução</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-            <Input 
-              placeholder="Busca por termos" 
-              className="col-span-4" 
-              value={termosBusca} 
-              onChange={(e) => setTermosBusca(e.target.value)} 
-            />                
+            <Select
+              value={tipo}
+              onValueChange={(value) => setTipo(value)}
+            >
+              <SelectTrigger  className="w-full sm:col-span-2">
+                <SelectValue placeholder="Selecione um tipo" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="todos">Todos os tipos</SelectItem>
+                <SelectItem value="constituicaoEstadual">Constituição Estadual</SelectItem>
+                <SelectItem value="decretoLegislativo">Decreto Legislativo</SelectItem>
+                <SelectItem value="decretoLei">Decreto Lei</SelectItem>
+                <SelectItem value="decretoNumerado">Decreto Numerado</SelectItem>
+                <SelectItem value="decretoNaoNumerado">Decreto Não Numerado</SelectItem>
+                <SelectItem value="emendaConsitucional">Emenda Constitucional</SelectItem>
+                <SelectItem value="instrucaoNormativa">Instrução Normativa</SelectItem>
+                <SelectItem value="leiComplementar">Lei Complementar</SelectItem>
+                <SelectItem value="leiOrdinaria">Lei Ordinária</SelectItem>
+                <SelectItem value="mensagemDoGovernador">Mensagem do Governador</SelectItem>
+                <SelectItem value="portaria">Portaria</SelectItem>
+                <SelectItem value="portariaConjunta">Portaria Conjunta</SelectItem>
+                <SelectItem value="resolucao">Resolução</SelectItem>
+              </SelectContent>
+            </Select>
+            <Input
+              placeholder="Busca por termos"
+              value={termosBusca}
+              onChange={(e) => setTermosBusca(e.target.value)}
+              className="w-full sm:col-span-2 md:col-span-4"
+            />
           </div>
-          
-          <div className="flex flex-row gap-4 items-center">
-            <Button 
-              type="button" 
-              variant="secondary" 
-              size="lg" 
+
+          <div className="flex flex-row gap-4 items-center mt-4">
+            <Button
+              type="button"
+              variant="secondary"
+              size="lg"
               onClick={handleSearch}
+              className="flex items-center"
             >
               <Search className="h-4 w-4 mr-2" />
               Pesquisar
             </Button>
-            <Button 
-              type="button" 
-              variant="outline" 
-              size="lg" 
+            <Button
+              type="button"
+              variant="outline"
+              size="lg"
               onClick={handleClearFilters}
+              className="flex items-center"
             >
               <X className="h-4 w-4 mr-2" />
               Remover filtros
-            </Button> 
+            </Button>
           </div>
         </div>
       ) : (
-        <AtosCardPublic /> // Passando os filtros para o AtosCard
+        <AtosCardPublic />
       )}
     </>
   );
