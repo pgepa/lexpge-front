21b0dbe7feac11bcb858a7141787f54bd4ec8c5d
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2024-08-22 13:25:50 -0300

    add função de imprimir o ato normativo

--- src/pages/_layouts/texto-integral.tsx
+++ src/pages/_layouts/texto-integral.tsx
@@ -1,13 +1,22 @@
-import { Outlet } from "react-router-dom"
-
+import { Outlet } from "react-router-dom";
+import { Printer } from 'lucide-react';
 
 export function TextoIntegralLayout() {
-  return (
-         
 
-      <div className="flex flex-col items-center justify-center relative">
-        <Outlet />
-      </div>
-    
-  )
+  const handlePrint = () => {
+    window.print();
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center relative">
+      <button
+        onClick={handlePrint}
+        className="absolute top-4 left-4 p-2 bg-gray-200 rounded-full hover:bg-gray-300"
+        title="Imprimir"
+      >
+        <Printer size={24} />
+      </button>
+      <Outlet />
+    </div>
+  );
 }
